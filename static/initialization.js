d99b7c2d4aa768f19546494aa3ab9bf2f143af97
dmaloparac9519rn <dmaloparac9519rn@raf.rs>
2022-01-15 17:20:41 +0100

    remove comment

--- static/initialization.js
+++ static/initialization.js
@@ -22,47 +22,6 @@ function init() {
               });
     });
 
-    // fetch('http://127.0.0.1:8000/users', {
-    //         method: 'GET',
-    //         headers: { 'Content-Type': 'application/json' }
-    //     })
-    //         .catch(err => {
-    //             res.status(403)
-    //         })
-    //           .then(el => {
-    //             if (el.status != 200) {
-    //                 // errorElement.innerText = "Forbidden";
-    //                 console.log("Forbidden")
-    //             } else {
-    //                 // var usersBtn = document.createElement("button");
-    //                 // usersBtn.id = "users";
-    //                 // usersBtn.className = "btn btn-primary";
-    //                 // usersBtn.innerText = "Users";
-    //                 // var container = document.getElementById("container");
-    //                 // container.insertBefore(usersBtn, errorElement);
-                    
-    //                 document.getElementById('usersBtn').addEventListener('click', e => {
-    //                     e.preventDefault();
-                        
-    //                     try {
-    //                         window.location.href = 'users.html';
-    //                     } catch(error) {
-    //                         res.status(404).send('Not found');
-    //                     }
-    //                 });
-    //             }
-    //         });
-
-    // document.getElementById('usersBtn').addEventListener('click', e => {
-    //     e.preventDefault();
-
-    //     try {
-    //         window.location.href = 'newUsers.html';
-    //     } catch(error) {
-    //         res.status(404).send('Not found');
-    //     }
-    // });
-
     document.getElementById('stations').addEventListener('click', e => {
         e.preventDefault();
 
